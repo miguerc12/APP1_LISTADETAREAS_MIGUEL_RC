8b2cf9a220a2a06ae6121beb61e964d8d3ed001f
miguerc12 <cobmigue329@gmail.com>
2024-03-15 16:29:37 -0600

    cree mi interfaz de la app

---
+++ app/src/main/java/rodriguez/miguel/app1_listadetareas_miguel_rc/MainActivity.java
@@ -0,0 +1,14 @@
+package rodriguez.miguel.app1_listadetareas_miguel_rc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
