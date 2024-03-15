2c3a8765c3e7e3f779eb86eab2f76886de3e22f4
miguerc12 <cobmigue329@gmail.com>
2024-03-15 16:32:23 -0600

    añadia la inializacion

--- app/src/main/java/rodriguez/miguel/app1_listadetareas_miguel_rc/MainActivity.java
+++ app/src/main/java/rodriguez/miguel/app1_listadetareas_miguel_rc/MainActivity.java
@@ -1,14 +1,45 @@
 package rodriguez.miguel.app1_listadetareas_miguel_rc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.os.Bundle;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTask;
+    private Button buttonAddTask;
+    private Button buttonSelectDeleteTask;
+    private ListView listViewTasks;
+    private List<String> taskList;
+    private ArrayAdapter<String> taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        editTextTask = findViewById(R.id.editTextTask);
+        buttonAddTask = findViewById(R.id.buttonAddTask);
+        buttonSelectDeleteTask = findViewById(R.id.buttonSelectDeleteTask);
+        listViewTasks = findViewById(R.id.listViewTasks);
+
+        taskList = new ArrayList<>();
+        taskAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
+        listViewTasks.setAdapter(taskAdapter);
+
+        
 }
+    }
